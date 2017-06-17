e0af8f3ee672b548fd243cc1f8d39677389ab632
MANOJ KUMAR PATRA <patra.manoj0@gmail.com>
2017-06-17 17:43:15 +0530

    Create binary_string_generation.py

---
+++ Recursion/binary_string_generation.py
@@ -0,0 +1,21 @@
+#generate binaary strings of n-bits
+def binary(mylist,n):
+    if n<1:
+        print(mylist)
+    else:
+        if len(mylist) != 0:
+            del mylist[n-1]
+        mylist.insert(n-1,0)
+        binary(mylist,n-1)
+        if len(mylist) != 0:
+            del mylist[n-1]
+        mylist.insert(n-1,1)
+        binary(mylist,n-1)
+
+def gen(num):
+    mylist = []
+    for x in range(0,num):
+        mylist.append(0)
+    binary(mylist, num)
+
+gen(2)
