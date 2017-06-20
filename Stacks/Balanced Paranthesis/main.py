57a5f3cdbe20536cd474a3b8a25bf001e46ab810
MANOJ KUMAR PATRA <patra.manoj0@gmail.com>
2017-06-20 06:06:44 +0530

    Create main.py

---
+++ Stacks/Balanced Paranthesis/main.py
@@ -0,0 +1,26 @@
+import stack
+
+def parChecker(symbolString):
+  s=Stack()
+  balanced=True
+  index=0
+  
+  while index < len(symbolString) and balanced:
+    symbol=symbolString[index]
+    if symbol="(":
+      s.push(symbol)
+     else:
+      if s.isEmpty():
+        balanced=False
+      else:
+        s.pop()
+    
+    index += 1
+    
+    if balanced and s.isEmpty():
+      return True
+    else:
+      return False
+      
+print(parChecker('((()))'))
+print(parChecker('(()'))
