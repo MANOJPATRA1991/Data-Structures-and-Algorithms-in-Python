4c9a1fd5d50d85e0387994b2bcd1b9b9071de868
MANOJ KUMAR PATRA <patra.manoj0@gmail.com>
2017-06-20 07:36:45 +0530

    Create main.py

---
+++ Stacks/Converting Decimal Numbers to Binary Numbers/main.py
@@ -0,0 +1,18 @@
+from Stacks.stack1 import Stack
+
+def divideBy2(decNumber):
+  remstack=Stack()
+  
+  while decNumber > 0:
+    rem=decNumber % 2
+    remstack.push(rem)
+    decNumber=decNumber // 2
+    
+  binString=""
+  
+  while not remstack.isEmpty():
+    binString=binString + str(remstack.pop())
+    
+  return binString
+  
+print(divideBy2(42))
