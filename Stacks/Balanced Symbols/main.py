852cc0045b40489344a0bea68aecb22e2889aba5
MANOJ KUMAR PATRA <patra.manoj0@gmail.com>
2017-06-20 06:44:42 +0530

    Create main.py

---
+++ Stacks/Balanced Symbols/main.py
@@ -0,0 +1,33 @@
+from Stacks.stack1 import Stack
+
+def parChecker(symbolString):
+  s=Stack()
+  balanced=True
+  index=0
+  
+  while index < len(symbolString)  and balanced:
+    symbol=symbolString[index]
+    if symbol in "([{":
+      s.push(symbol)
+    else:
+      if s.isEmpty():
+        balanced=False
+      else:
+        top=s.pop()
+        if not matches(top, symbol):
+          balanced=False
+    
+    index += 1
+    
+  if balanced and s.isEmpty():
+    return True
+  else:
+    return False
+    
+def matches(open, close):
+  opens="([{"
+  closers=")]}"
+  return opens.index(open) == closers.index(close)
+  
+print(parChecker('{{([][])}()}'))
+print(parChecker('[{()]'))
