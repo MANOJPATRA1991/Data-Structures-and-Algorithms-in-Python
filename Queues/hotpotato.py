7b0061469f7c2e1e4e87806c3b1eb192c30d3e9a
MANOJ KUMAR PATRA <patra.manoj0@gmail.com>
2017-06-22 09:32:40 +0530

    Create hotpotato.py

---
+++ Queues/hotpotato.py
@@ -0,0 +1,16 @@
+from Queues import queue
+
+def hotPotato(namelist, num):
+    simQueue=Queue()
+    for name in namelist:
+        simQueue.enqueue(name)
+
+    while simQueue.size() > 1:
+        for i in range(num):
+            simQueue.enqueue(simQueue.dequeue())
+
+        simQueue.dequeue()
+
+    return simQueue.dequeue()
+
+print(hotPotato(["Bill","David","Susan","Jane","Kent","Brad"],7))
