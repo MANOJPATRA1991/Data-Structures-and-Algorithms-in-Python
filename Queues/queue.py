07d7487231694f86ace74af304ab10bb9375b72d
MANOJ KUMAR PATRA <patra.manoj0@gmail.com>
2017-06-22 09:24:30 +0530

    Create queue.py

---
+++ Queues/queue.py
@@ -0,0 +1,27 @@
+# assume rear is at position 0 in the list
+
+class Queue(object):
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    # O(n) performance
+    def enqueue(self, items):
+        self.items.insert(0, items)
+
+    # O(1)
+    def dequeue(self):
+        self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+q=Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+print(q.items)
+q.dequeue()
+print(q.items)
