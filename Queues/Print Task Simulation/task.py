ce1a69cb18c7f4a9358451e81aff478e3da52c9c
MANOJ KUMAR PATRA <patra.manoj0@gmail.com>
2017-06-22 11:04:27 +0530

    Create task.py

---
+++ Queues/Print Task Simulation/task.py
@@ -0,0 +1,20 @@
+import random
+
+
+class Task(object):
+    def __init__(self, time):
+        self.timestamp = time
+        self.pages = random.randrange(1, 21)
+
+    def get_stamp(self):
+        return self.timestamp
+
+    def get_pages(self):
+        return self.pages
+
+    def wait_time(self, current_time):
+        """
+        :rtype: int
+        :type current_time: int
+        """
+        return current_time - self.timestamp
