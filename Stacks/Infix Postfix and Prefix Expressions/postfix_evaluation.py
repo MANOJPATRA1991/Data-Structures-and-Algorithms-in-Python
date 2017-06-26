4c38c044ad9892a056bd18fcaf737d4a1d3e5420
MANOJ KUMAR PATRA <patra.manoj0@gmail.com>
2017-06-26 11:25:43 +0530

    Update postfix_evaluation.py
    updated postfix expression method to include validation

--- Stacks/Infix Postfix and Prefix Expressions/postfix_evaluation.py
+++ Stacks/Infix Postfix and Prefix Expressions/postfix_evaluation.py
@@ -1,27 +1,56 @@
 from Stacks.stack1 import Stack
 
-def postfixEval(postfixexpr):
-  operandStack=Stack()
-  tokenList=postfixexpr.split()
-  
-  for token in tokenList:
-    if token in "0123456789":
-      operandStack.push(int(token))
-    else:
-      operand2=operandStack.pop()
-      operand1=operandStack.pop()
-      result=doMath(token, operand1, operand2)
-      operandStack.push(result)
-  return operandStack.pop()
+def postfix_eval(postfixexpr):
+    opStack = Stack()
+    tokenList = postfixexpr.split()
 
-def doMath(op, op1, op2):
-  if op == "*":
-    return op1*op2
-  elif op == "/":
-    return op1/op2
-  elif op == "+":
-    return op1+op2
-  else:
-    return op1-op2
-  
-print(postfixEval("7 8 + 3 2 + /"))
+    for token in tokenList:
+        if token in "0123456789":
+            opStack.push(int(token))
+        else:
+            # if opstack's size is 1 then do_math can't
+            # operate as it requires two operands
+            if opStack.size() < 2:
+                return None
+            operand2 = opStack.pop()
+            operand1 = opStack.pop()
+            result = do_math(token, operand1, operand2)
+            opStack.push(result)
+    # if opstack size is greater than 1
+    # then expression is missing an operator
+    if opStack.size() > 1:
+        return None
+    else:
+        return opStack.pop()
+
+
+def do_math(op, op1, op2):
+    if op == "*":
+        return op1 * op2
+    elif op == "/":
+        return op1 / op2
+    elif op == "+":
+        return op1 + op2
+    elif op == "-":
+        return op1 - op2
+    # if operator other than -,+,*,/
+    # return None
+    else:
+        return None
+
+
+def read_input():
+    print("Enter postfix expression separated by space.")
+    inp = input("Expression: ")
+    return inp
+
+
+def main():
+    postfix = read_input()
+    result = postfix_eval(postfix)
+    print("Result: ", result)
+
+
+if __name__ == '__main__':
+    main()
+
