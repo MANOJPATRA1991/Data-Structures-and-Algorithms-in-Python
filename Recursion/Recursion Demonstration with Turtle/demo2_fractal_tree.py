4f79f10dc8e1b0a63e770b417bf8c51128fa5c7c
MANOJ PATRA <patra.manoj0@gmail.com>
2017-07-02 09:12:48 +0530

    Update the Turtle Demo with different colors for Fractal Trees

--- Recursion/Recursion Demonstration with Turtle/demo2_fractal_tree.py
+++ Recursion/Recursion Demonstration with Turtle/demo2_fractal_tree.py
@@ -1,15 +1,20 @@
 import turtle
+from random import randint
 
 
-def tree(branch_len, t):
+def tree(branch_len, t, x):
     if branch_len > 5:
+        t.pensize(x)
+        if x == 2:
+            t.color("green")
         t.forward(branch_len)
         t.right(20)
-        tree(branch_len - 15, t)
+        tree(branch_len - 15, t, x-2)
         t.left(40)
-        tree(branch_len - 15, t)
+        tree(branch_len - 15, t, x-2)
         t.right(20)
         t.backward(branch_len)
+        t.color("brown")
 
 
 def main():
@@ -19,8 +24,9 @@ def main():
     t.up()
     t.backward(100)
     t.down()
-    t.color("green")
-    tree(75, t)
+    t.pensize(10)
+    t.color("brown")
+    tree(75, t, 10)
     myWin.exitonclick()
 
 
