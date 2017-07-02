112b74ded206609cfbdcafff3cd3e4396303b7cf
MANOJ KUMAR PATRA <patra.manoj0@gmail.com>
2017-07-02 07:46:03 +0530

    Fix bug returning None in some test cases
    1. Changed the base case
    2. Should return the recursion call

--- Recursion/palindrome_checker.py
+++ Recursion/palindrome_checker.py
@@ -1,10 +1,10 @@
 def is_palindrome(word):
 
     # base case
-    if len(word) <= 2 and word[0] == word[-1]:
+    if len(word) <= 1:
         return True
     elif word[0] == word[-1]:
-        is_palindrome(word[1:-1])
+        return is_palindrome(word[1:-1])
     else:
         return False
         
