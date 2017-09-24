9efc9a5d1c4ca69798c7c7c80af8991a1e23e0a3
MANOJ PATRA <patra.manoj0@gmail.com>
2017-09-24 11:31:51 +0530

    Create doubly linked list

--- Linked List/doubly_linked_list.py
+++ Linked List/doubly_linked_list.py
@@ -1,67 +1,69 @@
 class Element(object):
     def __init__(self, value):
-        self.value=value
-        self.next=None
-        self.prev=None
+        self.value = value
+        self.next = None
+        self.prev = None
+
 
 class DLL(object):
     def __init__(self, head=None):
-        self.head=head
+        self.head = head
 
     def append(self, new_element):
-        current=self.head
+        current = self.head
         if self.head:
             while current.next:
-                current= current.next
-            current.next=new_element
+                current = current.next
+            current.next = new_element
             new_element.prev = current
         else:
-            self.head=new_element
+            self.head = new_element
 
     def insert(self, new_element, position):
-        counter=1
-        current=self.head
+        counter = 1
+        current = self.head
         if position > 1:
             while current and counter < position:
-                if counter == position-1:
-                    new_element.next=current.next
-                    current.next.prev=new_element           
-                    current.next=new_element
-                    new_element.prev=current
-                current=current.next
+                if counter == position - 1:
+                    new_element.next = current.next
+                    current.next.prev = new_element
+                    current.next = new_element
+                    new_element.prev = current
+                current = current.next
                 counter += 1
         elif position == 1:
-            new_element.next=self.head
-            self.head.prev=new_element
-            self.head=new_element
+            new_element.next = self.head
+            self.head.prev = new_element
+            self.head = new_element
 
     def delete(self, value):
-        current=self.head
+        current = self.head
         while current.value != value and current.next:
-            current=current.next
+            current = current.next
         if current.value == value:
             if current.prev:
-                current.next.prev=current.prev
-                current.prev.next=current.next
-                current.prev=None
-                current.next=None
+                current.next.prev = current.prev
+                current.prev.next = current.next
+                current.prev = None
+                current.next = None
             else:
-                self.head=current.next
-                current.next.prev=None
-                current.next=None
-     
+                self.head = current.next
+                current.next.prev = None
+                current.next = None
+
     def get_position(self, position):
-        counter=1
-        current=self.head
+        counter = 1
+        current = self.head
         if position < 1:
             return None
         while current and counter <= position:
             if counter == position:
                 return current
-            current=current.next
+            current = current.next
             counter += 1
         return None
-    
+
+
 e1 = Element(1)
 e2 = Element(2)
 e3 = Element(3)
@@ -71,7 +73,7 @@ dll = DLL(e1)
 dll.append(e2)
 dll.append(e3)
 
-dll.insert(e4,3)
+dll.insert(e4, 3)
 
 dll.delete(1)
 print(dll.get_position(1).value)
