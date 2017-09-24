314628f4594d21e3b3ec6c70084e3ce39306be22
MANOJ PATRA <patra.manoj0@gmail.com>
2017-09-24 11:32:54 +0530

    Create queue data structure

--- Queues/queue.py
+++ Queues/queue.py
@@ -18,10 +18,10 @@ class Queue(object):
     def size(self):
         return len(self.items)
 
-q=Queue()
-q.enqueue(1)
-q.enqueue(2)
-q.enqueue(3)
-print(q.items)
-q.dequeue()
-print(q.items)
+# q=Queue()
+# q.enqueue(1)
+# q.enqueue(2)
+# q.enqueue(3)
+# print(q.items)
+# q.dequeue()
+# print(q.items)
