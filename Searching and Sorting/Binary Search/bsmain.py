1606813582dd95a22f91c5cf944c25854087e4cd
MANOJ PATRA <patra.manoj0@gmail.com>
2017-09-24 11:29:04 +0530

    Implement binary search

--- Searching and Sorting/Binary Search/bsmain.py
+++ Searching and Sorting/Binary Search/bsmain.py
@@ -16,5 +16,5 @@ def binary_search(input_array, value):
 test_list = [1,3,9,11,15,19,29]
 test_val1 = 25
 test_val2 = 15
-print binary_search(test_list, test_val1)
-print binary_search(test_list, test_val2)
+print (binary_search(test_list, test_val1))
+print (binary_search(test_list, test_val2))
