d97c22bcef0205fadd10d88846f572510877e6bb
Manoj Kumar Patra <patra.manoj0@gmail.com>
2017-10-17 23:19:30 +0530

    Add new instance variables for DFS

--- Graphs/Vertex/main.py
+++ Graphs/Vertex/main.py
@@ -5,10 +5,14 @@ class Vertex:
         id(int): The id of the vertex
         connectedTo(dict): Dictionary to keep track of the vertices
                         to which it is connected and the weight of each edge
-        distance(int): Distance from the starting vertex
+        distance(int): Distance from the starting vertex used in BFS
         color(str): Color of the Vertex that specifies unexplored,
-                    partially explored or completely explored
-        predecessor(Vertex): The predecessor of this Vertex
+                    partially explored or completely explored, used in 
+                    BFS and DFS
+        predecessor(Vertex): The predecessor of this Vertex, used in
+                    BFS and DFS
+        discovery(int): The time taken to discover a Vertex, used in DFS
+        finish(int): The time taken to finish exploring a Vertex, used in DFS
     """
 
     def __init__(self, key):
@@ -17,6 +21,8 @@ class Vertex:
         self.distance = 0
         self.color = "white"
         self.predecessor = None
+        self.discovery = 0
+        self.finish = 0
 
     def addNeighbor(self, nbr, weight=0):
         """
@@ -28,6 +34,7 @@ class Vertex:
         """
         self.connectedTo[nbr] = weight
 
+        
     def __str__(self):
         """
         Returns string representation of vertices connected to this vertex
@@ -35,6 +42,7 @@ class Vertex:
         return str(self.id) + ' connectedTo: ' + \
             str([x.id for x in self.connectedTo])
 
+        
     def getConnections(self):
         """
         Returns all of the vertices in the adjacency list
@@ -44,6 +52,7 @@ class Vertex:
         """
         return self.connectedTo.keys()
 
+    
     def getId(self):
         """
         Returns the id of this vertex
@@ -52,6 +61,7 @@ class Vertex:
         """
         return self.id
 
+    
     def getWeight(self, nbr):
         """
         Returns the weight of the edge from this
@@ -63,20 +73,42 @@ class Vertex:
         """
         return self.connectedTo[nbr]
 
+    
     def setDistance(self, n):
         self.distance = n
 
+    
     def getDistance(self):
         return self.distance
 
+    
     def setColor(self, color):
         self.color = color
 
+    
     def getColor(self):
         return self.color
 
+    
     def setPredecessor(self, v):
         self.predecessor = v
 
+    
     def getPredecessor(self):
         return self.predecessor
+
+    
+    def setDisocvery(self, t):
+        self.discovery = t
+        
+        
+    def getDisocvery(self):
+        return self.discovery    
+    
+    
+    def setFinish(self, t):
+        self.finish = t
+        
+        
+    def getFinish(self):
+        return self.finish
