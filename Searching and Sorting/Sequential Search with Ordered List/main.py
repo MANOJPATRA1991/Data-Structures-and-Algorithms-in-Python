88ad1554dfd588c3282b39ce5f613587260d8c75
Manoj Kumar Patra <patra.manoj0@gmail.com>
2019-10-30 08:37:29 +0530

    Add comments

--- Searching and Sorting/Sequential Search with Ordered List/main.py
+++ Searching and Sorting/Sequential Search with Ordered List/main.py
@@ -7,21 +7,30 @@ def ordered_sequential_search(alist, item):
     Returns:
         Boolean: Indicates if item is found or not
     """
-    # Keep track of the position of the item
+    # Keeps track of the position of the item
     pos = 0
+    # Keeps track of item's discovery
     found = False
+    # stop is True if item at pos is greater than the searched item
     stop = False
+    
+    # Keep searching if pos is valid and
+    # item is not found and 
+    # next element in the list is less than the searched item
     while pos < len(alist) and not found and not stop:
         if alist[pos] == item:
+            # Item found
             found = True
         else:
-            # Check if item searched is greater than item at a particular index
-            # Stop if true means item searched for is not in the list
+            # Check if item at index 'pos' is greater than searched item
             if alist[pos] > item:
+                # Stop as searched item is not in the list
                 stop = True
             else:
+                # Item not found and item at 'pos' is less than searched item 
+                # => move to next position
                 pos = pos+1
-    # Item not found
+
     return found
 
 testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42]
